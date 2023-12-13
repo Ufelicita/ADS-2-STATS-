f7f4f633e8f0779632fd97dbacbd8f1f6eed3b01
Ufelicita <148863590+Ufelicita@users.noreply.github.com>
2023-12-13 15:47:33 +0000

    Update ADS assign2.py

--- ADS assign2.py
+++ ADS assign2.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
 """
 
-The code  analyzes World Bank data for 10 European countries
-regarding climate change using four indicators: Nitrous Oxide  emissions,
-agricultural land area, population, and life expectancy over 25 years.
+The code  analyzes World Bank data for 10  countries regarding 
+climate change using four indicators: Forest Area, GDP, Population,
+Total Greenhouse emmisions (kt of co2), Manufacturing Value Added.
+over 25 years.
 
  Created December 1, 2023
 
@@ -37,46 +37,61 @@ def read(filename):
     # Read csv file
     df = pd.read_csv(filename)
 
-    # Drop columns not required
-    df = df.drop(["Series Code", "Country Code"],axis=1)
-    
-    # Rename Columns names 
-    df.columns = ["Indicator Name","Country Name","2003", "2004", "2005", 
-                  "2006","2007","2008","2009", "2010", "2011", "2012", 
-                  "2013", "2014", "2015", "2016", "2017", "2018","2019", 
-                  "2020"]
-    
-   
-    
-    #set Coountry Name as index 
-    df = df.set_index(["Country Name", "Indicator Name"])
-  
-   
-    
-    # Transpose df so that countries become columns 
+    # Rename Columns names
+    df.columns = ["Country Name", "Indicator Name", "Series Code",
+                  "Country Code", "1991",  "1992", "1993", "1994", "1995",
+                  "1996", "1997", "1998", "1999", "2000", "2001", "2002",
+                  "2003", "2004", "2005", "2006", "2007", "2008", "2009",
+                  "2010", "2011",  "2012", "2013", "2014", "2015"]
+
+    # set Coountry Name as index
+    #df = df.set_index(["Country Name", "Indicator Name"])
+
+    # Transpose df so that countries become columns
     df_t = df.transpose()
-    
-    #check for missing data 
-    print(df_t.isna().any())
-    
+
+    # Set Country names as header
+    df_t.columns = df_t.iloc[0]
+
+    # Drop unwanted rows
+    df_t = df_t.drop(["Country Name", "Series Code", "Country Code"], axis=0)
+
+    # check for missing values
+    df_t.isna().any()
+
+    # Check the class type of data
+    print(df_t)
+    print(type(df_t.index[1]))
+    print(type(df_t.iloc[1, 1]))
+
+    # Format DataFrame so that values show properly
+    df_t_formats = {"South Africa": "{:9.0f}".format,
+                    "India": "{:9.0f}".format,
+                    "korea Rep": "{:9.0f}".format,
+                    "United Kingdom": "{:9.0f}".format,
+                    "Rwanda": "{:9.0f}".format,
+                    "Saudi Arabia": "{:9.0f}".format,
+                    "Netherlands": "{:9.0f}".format,
+                    "Guyana": "{:9.0f}".format, "Chile": "{:9.0f}".format,
+                    "Germany": "{:9.0f}".format}
+
+    df_t.head().style.format(df_t_formats)
+    print(df_t_formats)
+
+    # Set Dataframe to standard float format
+    textfile = open("country.txt", "w")
+    df_t.to_string(textfile, formatters=df_t_formats)
+
+    textfile.close()
+
     return df, df_t
 
+# Check the class type of data
+# print(df_t_sub)
+# print(type(df_t_sub.index[1]))
+#print(type(df_t_sub.iloc[1, 1]))
+#
+
+
 # Analyse the DataFrame
-df, df_t = read("worldbank.csv")
-
-# Statis
-df_t_decribe_stat = df_t.describe()
-print(df_t_decribe_stat)
-print(df_t.dtypes)
-
-correlation = df_t.corr()
-
-for series in df_t.columns.levels[1]:
-    plt.figure()
-    fig, ax = plt.subplots(figsize=(10, 10))
-    df_t.xs(series,level=1, axis=1).plot(ax=ax)
-    ax.set_ylabel(series)
-    plt.show()
-    
-
-
+df, df_t = read("worldbankdata.csv")
