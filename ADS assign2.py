f0b6eafa9c4a36057791265afdf48bb99ab741b4
Ufelicita <148863590+Ufelicita@users.noreply.github.com>
2023-12-14 16:07:03 +0000

    Add files via upload

--- ADS assign2.py
+++ ADS assign2.py
@@ -1,15 +1,17 @@
+# -*- coding: utf-8 -*-
 """
 
-The code  analyzes World Bank data for 10  countries regarding 
-climate change using four indicators: Forest Area, GDP, Population,
+The code  analyzes World Bank data for 10  countries regarding
+climate change using five indicators: Forest Area, GDP, Population,
 Total Greenhouse emmisions (kt of co2), Manufacturing Value Added.
-over 25 years.
+ every 6 years for 24 years .
 
  Created December 1, 2023
 
 
 @author: Felicita Adeleke (Student Id : )
 """
+
 
 # Import necessary libraries
 import pandas as pd
@@ -19,79 +21,108 @@ import seaborn as sns
 from scipy.stats import kendalltau
 
 
-def read(filename):
+def read(filename, index_col):
     """
-    Reads the DataFrame in world bank format and returns two data frames with 
-    years as columns and countries as columns 
+    Reads the DataFrame in world bank format and returns two data frames with
+    years as columns and countries as columns
 
-    Args: 
+    Args:
     ----------
 
     filename(str): The name of the file with the DataFrame
 
     Returns:
-        years_df A :Transposed dataframe with years  as columns 
+        years_df A :Transposed dataframe with years  as columns
         countries_df: Transposed dataframe with countries as columns
     """
-
     # Read csv file
     df = pd.read_csv(filename)
 
-    # Rename Columns names
-    df.columns = ["Country Name", "Indicator Name", "Series Code",
-                  "Country Code", "1991",  "1992", "1993", "1994", "1995",
-                  "1996", "1997", "1998", "1999", "2000", "2001", "2002",
-                  "2003", "2004", "2005", "2006", "2007", "2008", "2009",
-                  "2010", "2011",  "2012", "2013", "2014", "2015"]
-
-    # set Coountry Name as index
-    #df = df.set_index(["Country Name", "Indicator Name"])
+    # set index
+    df.set_index(index_col, inplace=True)
 
     # Transpose df so that countries become columns
     df_t = df.transpose()
 
-    # Set Country names as header
-    df_t.columns = df_t.iloc[0]
-
-    # Drop unwanted rows
-    df_t = df_t.drop(["Country Name", "Series Code", "Country Code"], axis=0)
-
-    # check for missing values
-    df_t.isna().any()
-
-    # Check the class type of data
-    print(df_t)
-    print(type(df_t.index[1]))
-    print(type(df_t.iloc[1, 1]))
-
-    # Format DataFrame so that values show properly
-    df_t_formats = {"South Africa": "{:9.0f}".format,
-                    "India": "{:9.0f}".format,
-                    "korea Rep": "{:9.0f}".format,
-                    "United Kingdom": "{:9.0f}".format,
-                    "Rwanda": "{:9.0f}".format,
-                    "Saudi Arabia": "{:9.0f}".format,
-                    "Netherlands": "{:9.0f}".format,
-                    "Guyana": "{:9.0f}".format, "Chile": "{:9.0f}".format,
-                    "Germany": "{:9.0f}".format}
-
-    df_t.head().style.format(df_t_formats)
-    print(df_t_formats)
-
-    # Set Dataframe to standard float format
-    textfile = open("country.txt", "w")
-    df_t.to_string(textfile, formatters=df_t_formats)
-
-    textfile.close()
-
     return df, df_t
-
-# Check the class type of data
-# print(df_t_sub)
-# print(type(df_t_sub.index[1]))
-#print(type(df_t_sub.iloc[1, 1]))
-#
 
 
 # Analyse the DataFrame
-df, df_t = read("worldbankdata.csv")
+df, df_t = read("worldbankdata.csv", index_col=["Country Name", "Series Name"])
+
+# Drop unnessary columns
+df_t = df_t.drop(["Series Code", "Country Code"], axis=0)
+
+# Check Data Type
+print(df_t.dtypes)
+
+# change the data type
+df_t = df_t.apply(pd.to_numeric, errors="coerce")
+print(df_t.dtypes)
+
+# check for NaN
+print(df_t.isna().sum())
+
+# Change years format
+df_t_yr_format = {"1995 [YR1995]": "1995", "2000 [YR2000]": "2000",
+                  "2005 [YR2005]": "2005", "2010 [YR2010]": "2010",
+                  "2010 [YR2010]": "2010"}
+
+print(df_t.rename(df_t_yr_format,axis=0))
+
+# Format DataFrame so that values show properly
+df_t_formats = {"South Africa": "{:9.0f}".format, "India": "{:9.0f}".format,
+                "korea Rep": "{:9.0f}".format, "Rwanda": "{:9.0f}".format,
+                "United Kingdom": "{:9.0f}".format, "Guyana": "{:9.0f}".format,
+                "Saudi Arabia": "{:9.0f}".format, "Germany": "{:9.0f}".format,
+                "Netherlands": "{:9.0f}".format}
+
+print(df_t.to_string(formatters=df_t_formats))
+
+
+"""
+Below is the summary statistics for three(3)countries :United Kingdom,
+South Africa and India for all the indicators, Population total, 
+GDP (current US$), Forest area (sq. km),Total greenhouse gas emissions
+(kt of CO2 equivalent)
+
+"""
+          
+# Calculate descriptive analysis for United Kindgom             
+uk_descb = df_t.xs("United Kingdom", level=0, axis=1).agg("describe")
+
+# Calculate Skewness  for United Kingdom 
+uk_skew = df_t.xs("United Kingdom", level=0, axis=1).agg("skew")
+
+
+# Calculate Kurtosis for United Kingdom 
+uk_kurtosis = df_t.xs("United Kingdom", level=0, axis=1).agg("kurtosis")
+
+      
+# Calculate descriptive analysis for India 
+India_descb = df_t.xs("India", level=0, axis=1).agg("describe")
+
+# Calculate skewness for India 
+India_Skew = df_t.xs("India", level=0, axis=1).agg("skew")
+
+# Calculate kurtosis for India 
+India_kurtosis = df_t.xs("India", level=0, axis=1).agg("skew") 
+
+# Calculate kurtosis  for South
+South_Africa_kurtosis = df_t.xs("South Africa", level=0,
+                                axis=1).agg("kurtosis") 
+
+# Calculate kurtosis  for South
+South_Africa_describe = df_t.xs("South Africa", level=0,
+                                axis=1).agg("describe")  
+                                               
+# Calculate  analysis for India 
+South_Africa__describe= df_t.xs("South Africa", level=0,
+                                axis=1).agg("describe")
+
+# Calculate skewness for India 
+South_Africa_skew = df_t.xs("South Africa", level=0, axis=1).agg("skew")  
+
+
+# Calculate kurtosis  for India 
+India_descb = df_t.xs("India", level=0, axis=1).agg("kurtosis")  
